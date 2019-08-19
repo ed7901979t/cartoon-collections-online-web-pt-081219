b383200323f01ca7d09a4d0f91a5af8b87f6fff5
clean-command-1851 <ed7901979t@gmail.com>
2019-08-19 20:54:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   index = 0   
-  array.each_with_index { |name| index 
+  array.each_with_index { |name, index | puts "" 
   # Your code here
   dwarves = %w[name]
   dwarves.each_with_index { |name| index}
